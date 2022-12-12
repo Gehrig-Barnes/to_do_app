1bde95ce9f73273115ea73a8fe97d8d94d8a4085
gehrig <gehrigbarnes@gmail.com>
2022-12-12 11:30:12 -0600

    gave my nav links a background color on click. Also add some transions so the color eases in

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,27 +1,54 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 function NavBar({ handleLogOutClick, user, artist }) {
+  const onNavClick = ({ isActive }) => ({
+    transition: "background-color 0.3s ease",
+    color: isActive ? "#fff" : "#545e6f",
+    background: isActive ? "#7600dc" : "#fff",
+  })
+  
   const showLogOutButton =
     user || artist ? (
-      <button onClick={handleLogOutClick}>Logout</button>
+      <button className="navLink" onClick={handleLogOutClick}>
+        Logout
+      </button>
     ) : (
-      <NavLink to="/login">LOGIN</NavLink>
+      <NavLink className="navLink" to="/login" style={onNavClick}>
+        LOGIN
+      </NavLink>
     );
 
+    
+
   return (
-    <div>
+    <div className="navBar">
       {!user && !artist ? (
         <>
           {" "}
-          <NavLink to="/">HOME</NavLink>
-          <NavLink to="/explore">EXPLORE</NavLink>
+          <NavLink
+            style={onNavClick}
+            className="navLink"
+            to="/"
+          >
+            HOME
+          </NavLink>
+          <NavLink  style={onNavClick} className="navLink" to="/explore">
+            EXPLORE
+          </NavLink>
         </>
       ) : (
         <>
-          <NavLink to="/">HOME</NavLink>
-          <NavLink to="/explore">EXPLORE</NavLink>
-          <NavLink to="/collection">{user ? "My Rentals" : "My Art"}</NavLink>
+          <NavLink  style={onNavClick} className="navLink" to="/">
+            HOME
+          </NavLink>
+          <NavLink  style={onNavClick} className="navLink" to="/explore">
+            EXPLORE
+          </NavLink>
+          <NavLink  style={onNavClick} className="navLink" to="/collection">
+            {user ? "My Rentals" : "My Art"}
+          </NavLink>
         </>
       )}
 
