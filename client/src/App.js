a221cd170f4111927a6dd816f9e5189133a2627d
gehrig <gehrigbarnes@gmail.com>
2022-12-11 19:25:56 -0600

    organizing routes based on the type of user that's logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,26 @@
 import React, { useEffect, useState } from "react";
-import Login from './components/Login/Login';
+import Login from "./components/Login/Login";
 import { Routes, Route, useNavigate } from "react-router-dom";
-import NavBar from './components/NavBar/NavBar';
+import NavBar from "./components/NavBar/NavBar";
 import Explore from "./components/Explore/Explore";
 import HomePage from "./components/HomePage/HomePage";
 import Collection from "./components/Collection/Collection";
+import ArtistCollection from "./components/ArtistCollection/ArtistCollection";
 
 function App() {
   const [user, setUser] = useState(null);
   const [artist, setArtist] = useState(null);
-  const navigate = useNavigate()
-
-  
-
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // auto-login
     fetch("/me").then((r) => {
       if (r.ok) {
         r.json().then((user) => setUser(user));
       }
     });
   }, []);
+
   useEffect(() => {
-    // auto-login
     fetch("/artist_me").then((r) => {
       if (r.ok) {
         r.json().then((user) => setArtist(user));
@@ -31,40 +28,51 @@ function App() {
     });
   }, []);
 
-  function handleLogOutClick(){
-    if(user){
-    fetch("/logout",{
-        method: "DELETE"
-    }).then((r) => {
-        if(r.ok){
-            setUser(null);
+  function handleLogOutClick() {
+    if (user) {
+      fetch("/logout", {
+        method: "DELETE",
+      }).then((r) => {
+        if (r.ok) {
+          setUser(null);
         }
-    });
-    // Navigate to home page after logout and clear history
-    navigate("/login");
-  } else {
-    fetch("/artist_logout",{
-      method: "DELETE"
-  }).then((r) => {
-      if(r.ok){
+      });
+      navigate("/login");
+    } else {
+      fetch("/artist_logout", {
+        method: "DELETE",
+      }).then((r) => {
+        if (r.ok) {
           setArtist(null);
-      }
-  });
-  // Navigate to home page after logout and clear history
-  navigate("/login");
+        }
+      });
+
+      navigate("/login");
+    }
   }
-}
+
   return (
-    
     <div className="App">
-      
-     <NavBar user={user} artist={artist} handleLogOutClick={handleLogOutClick} />
-     <Routes>
-      <Route path="/login" element={<Login onLogin={setUser} setArtist={setArtist}/>}/>
-      <Route path="/explore" element={<Explore/>}/>
-      <Route path="/collection" element={<Collection/>}/>
-      <Route path="/" element={<HomePage/>}/>
-     </Routes>
+      <NavBar
+        user={user}
+        artist={artist}
+        handleLogOutClick={handleLogOutClick}
+      />
+
+      <Routes>
+        {user || artist ? null : (
+          <Route
+            path="/login"
+            element={<Login onLogin={setUser} setArtist={setArtist} />}
+          />
+        )}
+        <Route path="/explore" element={<Explore />} />
+        <Route
+          path="/collection"
+          element={user ? <Collection /> : <ArtistCollection />}
+        />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -2,16 +2,29 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 function NavBar({ handleLogOutClick, user, artist }) {
-  const showLogOutButton = user || artist ? (
-    <button onClick={handleLogOutClick}>Logout</button>
-  ) : (
-    <NavLink to="/login">LOGIN</NavLink>
-  );
+  const showLogOutButton =
+    user || artist ? (
+      <button onClick={handleLogOutClick}>Logout</button>
+    ) : (
+      <NavLink to="/login">LOGIN</NavLink>
+    );
+
   return (
     <div>
-      <NavLink to="/">HOME</NavLink>
-      <NavLink to="/explore">EXPLORE</NavLink>
-      <NavLink to="/collection">My Collection</NavLink>
+      {!user && !artist ? (
+        <>
+          {" "}
+          <NavLink to="/">HOME</NavLink>
+          <NavLink to="/explore">EXPLORE</NavLink>
+        </>
+      ) : (
+        <>
+          <NavLink to="/">HOME</NavLink>
+          <NavLink to="/explore">EXPLORE</NavLink>
+          <NavLink to="/collection">{user ? "My Rentals" : "My Art"}</NavLink>
+        </>
+      )}
+
       {showLogOutButton}
     </div>
   );
